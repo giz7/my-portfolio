7d644b96a9ff5b69216e12916a80b431c94bdff1
Gustavo <gustavosizidio@gmail.com>
2025-04-08 17:33:58 -0300

    feat: creates Hero component UI

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -1,18 +1,24 @@
-import { Button, Container, Grid, styled, Typography } from "@mui/material";
+import { Box, Container, Grid, styled, Typography } from "@mui/material";
 import Avatar from "../../../../assets/images/avatar.jpg";
 import DownloadIcon from '@mui/icons-material/Download';
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import StyledButton from "../../../../components/StyledButton/StyledButton";
+import theme from "../../../../theme";
+import { AnimatedBackground } from "../../../../components/StyledButton/AnimatedBackground";
 
 const Hero = () => {
 
-    const StyledHero = styled("div")(() => ({
-        backgroundColor: "black",
+    const StyledHero = styled("div")(({theme}) => ({
+        backgroundColor: theme.palette.primary.main,
         height: "100vh",
+        display: "flex",
+        alignItems: "center",
     }))
     
     const StyledImg = styled("img")(() => ({
-        width: "100%",
+        width: "80%",
         borderRadius: "50%",
+        border: `1px solid ${theme.palette.primary.contrastText}`
     }))
 
 
@@ -21,24 +27,31 @@ const Hero = () => {
         <StyledHero>
             <Container maxWidth="lg">
                 <Grid container spacing={2}>
-                    <Grid size={{xs: 12, md:4}}>
-                        <StyledImg src={Avatar} />
+                    <Grid size={{xs: 12, md:5}}>
+                        <Box position="relative">
+                            <Box position="absolute" width={"150%"} top={-100} right={0}>
+                                <AnimatedBackground />
+                            </Box>
+                            <Box position="relative" textAlign="center">
+                                <StyledImg src={Avatar} />
+                            </Box>
+                        </Box>
                     </Grid>
-                    <Grid size={{xs: 12, md:8}}>
-                        <Typography color="primary" variant="h1" textAlign="center">Gusta Dev</Typography>
-                        <Typography color="primary" variant="h2" textAlign="center">I'm a Front-end Developer</Typography>
-                        <Grid container display="flex" justifyContent="center">
+                    <Grid size={{xs: 12, md:7}}>
+                        <Typography color="primary.contrastText" variant="h1" textAlign="center" pb={2}>Gusta Dev</Typography>
+                        <Typography color="primary.contrastText" variant="h2" textAlign="center">I'm a Front-end Developer</Typography>
+                        <Grid container display="flex" justifyContent="center" spacing={3} pt={3}>
                             <Grid size={{xs: 12, md:4}} display="flex" justifyContent="center">
-                                <Button>
+                                <StyledButton>
                                     <DownloadIcon/> 
-                                    Download CV
-                                </Button> 
+                                <Typography>Download CV</Typography>    
+                                </StyledButton>
                             </Grid>                            
                             <Grid size={{xs: 12, md:4}} display="flex" justifyContent="center">                                             
-                                <Button>
+                                <StyledButton>
                                     <MailOutlineIcon/>
-                                    Contact Me
-                                </Button>
+                                    <Typography>Contact Me</Typography>
+                                </StyledButton>
                             </Grid>                                             
                         </Grid>
                     </Grid>
